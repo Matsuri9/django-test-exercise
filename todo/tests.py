ff5152ef341efd82c70d453597c61782e251767d
1F10230183 <s1F102301831@iniad.org>
2024-07-24 23:07:08 +0900

    fix&add: TestCase

--- todo/tests.py
+++ todo/tests.py
@@ -68,16 +68,20 @@ class TodoViewTestCase(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
-        self.assertEqual(len(response.context['tasks']), 0)
+        self.assertEqual(len(response.context['uncompleted_tasks']), 0)
+        self.assertEqual(len(response.context['completed_tasks']), 0)
     
     def test_index_post(self):
         client = Client()
-        data = {'title': 'Test Task', 'due_at': '2024-06-30 23:59:59', 'priority': '5'}
+        data = {'title': 'Test Task', 'due_at': '2024-06-30T23:59:59', 'priority': '5'}
         response = client.post('/', data)
 
+        self.assertEqual(response.status_code, 302)
+        response = client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
-        self.assertEqual(len(response.context['tasks']), 1)
+        self.assertEqual(len(response.context['uncompleted_tasks']), 1)
+        self.assertEqual(len(response.context['completed_tasks']), 0)
     
     def test_index_get_order_post(self):
         task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
@@ -89,8 +93,8 @@ class TodoViewTestCase(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
-        self.assertEqual(response.context['tasks'][0], task2)
-        self.assertEqual(response.context['tasks'][1], task1)
+        self.assertIn(task1, response.context['uncompleted_tasks'])
+        self.assertIn(task2, response.context['uncompleted_tasks'])
 
     def test_index_get_order_due(self):
         task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
@@ -102,21 +106,21 @@ class TodoViewTestCase(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
-        self.assertEqual(response.context['tasks'][0], task1)
-        self.assertEqual(response.context['tasks'][1], task2)
+        self.assertEqual(response.context['uncompleted_tasks'][0], task1)
+        self.assertEqual(response.context['uncompleted_tasks'][1], task2)
     
     def test_index_get_order_priority(self):
-        task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)), priority="1")
+        task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)), priority=1)
         task1.save()
-        task2 = Task(title='task2', due_at=timezone.make_aware(datetime(2024, 7, 1)), priority="2")
+        task2 = Task(title='task2', due_at=timezone.make_aware(datetime(2024, 7, 1)), priority=2)
         task2.save()
         client = Client()
-        response = client.get('/?order=due')
+        response = client.get('/?order=priority')
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
-        self.assertEqual(response.context['tasks'][0], task1)
-        self.assertEqual(response.context['tasks'][1], task2)       
+        self.assertEqual(response.context['uncompleted_tasks'][0], task2)
+        self.assertEqual(response.context['uncompleted_tasks'][1], task1)
 
     def test_detail_get_success(self):
         task = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
@@ -128,9 +132,8 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.templates[0].name, 'todo/detail.html')
         self.assertEqual(response.context['task'], task)
     
-    def test_detail_get_fall(self):
+    def test_detail_get_fail(self):
         client = Client()
         response = client.get('/1/')
 
         self.assertEqual(response.status_code, 404)
-        
