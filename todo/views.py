e05e16fc34ba94fc9d14d5d727d4bfc7f8a963cf
1F10230183 <s1F102301831@iniad.org>
2024-07-24 23:02:09 +0900

    add: isCompleted ?&regarding CSS

--- todo/views.py
+++ todo/views.py
@@ -9,25 +9,27 @@ def index(request):
     if request.method == 'POST':
         task = Task(title=request.POST['title'], due_at=make_aware(parse_datetime(request.POST['due_at'])), priority=int(request.POST['priority']))
         task.save()
-    
-    if request.GET.get('order') == 'due':
-        tasks = Task.objects.order_by('due_at')
-    
-    elif request.GET.get('order') == 'priority':
-        tasks = Task.objects.order_by('-priority')
-    
+
+    order_by = request.GET.get('order', 'posted_at')
+    if order_by == 'due':
+        uncompleted_tasks = Task.objects.filter(completed=False).order_by('due_at')
+        completed_tasks = Task.objects.filter(completed=True).order_by('due_at')
+    elif order_by == 'priority':
+        uncompleted_tasks = Task.objects.filter(completed=False).order_by('-priority')
+        completed_tasks = Task.objects.filter(completed=True).order_by('-priority')
     else:
-        tasks = Task.objects.order_by('-posted_at')
-        
+        uncompleted_tasks = Task.objects.filter(completed=False).order_by('-posted_at')
+        completed_tasks = Task.objects.filter(completed=True).order_by('-posted_at')
+
     context = {
-        'tasks': tasks
+        'uncompleted_tasks': uncompleted_tasks,
+        'completed_tasks': completed_tasks,
     }
     return render(request, 'todo/index.html', context)
 
 def detail(request, task_id):
     try:
         task = Task.objects.get(pk=task_id)
-    
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
 
@@ -45,7 +47,6 @@ def delete(request, task_id):
     task.delete()
     return redirect(index)
 
-
 def update(request, task_id):
     try:
         task = Task.objects.get(pk=task_id)
@@ -57,7 +58,7 @@ def update(request, task_id):
         task.priority = int(request.POST['priority'])
         task.save()
         return redirect(detail, task_id)
-    
+
     context = {
         'task': task
     }
@@ -65,9 +66,9 @@ def update(request, task_id):
 
 def close(request, task_id):
     try:
-        task=Task.objects.get(pk=task_id)
+        task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
-    task.completed=True
+    task.completed = True
     task.save()
     return redirect(index)
